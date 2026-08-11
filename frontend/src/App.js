e04400acf873a7c07b28bf0b0a74a84a2f972f28
jigar-tecmantras <73215263+jigar-tecmantras@users.noreply.github.com>
2026-08-11 16:00:33 +0530

    Add React register page frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Register from './components/Register';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-shell">
+      <main className="app-shell__content">
+        <Register />
+      </main>
+    </div>
+  );
+}
+
+export default App;
